0a5ed3a675d23824ac27281d0a2ab3657553d9a8
natsukium <tm.tomatoes@gmail.com>
2017-12-25 15:00:59 +0900

    Move tmux config`

---
+++ tmux/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Install Plugin Manager for Tmux, TPM
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+echo "Please press Ctrl+q + I. then Installing plugins starts"
