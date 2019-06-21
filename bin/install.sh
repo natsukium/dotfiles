10cc18ee08878171a7afcb4ef1185c3e1f5eec8e
natsukium <tm.tomatoes@gmail.com>
2019-06-22 05:20:37 +0900

    Add installer

---
+++ bin/install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#     ____           __        ____        __
+#    /  _/___  _____/ /_____ _/ / /  _____/ /_
+#    / // __ \/ ___/ __/ __ `/ / /  / ___/ __ \
+#  _/ // / / (__  ) /_/ /_/ / / /_ (__  ) / / /
+# /___/_/ /_/____/\__/\__,_/_/_/(_)____/_/ /_/
+
+if type git >/dev/null 2>&1; then
+    git clone https://github.com/natsukium/dotfiles $HOME/.dotfiles
+else
+    tarball="https://github.com/natsukium/dotfiles/archive/master.tar.gz"
+    curl -L "$tarball" | tar xvz
+    mv -f dotfiles-master $HOME/.dotfiles
+fi
+
+cd $HOME/.dotfiles
+make minimum
