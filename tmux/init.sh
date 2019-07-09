14314e5eb3c836d09d643dee842b4da5f6b919bf
natsukium <tm.tomatoes@gmail.com>
2019-07-09 14:26:51 +0900

    Update tpm

--- tmux/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Install Plugin Manager for Tmux, TPM
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-echo "Please press prefix + I. then Installing plugins starts"
