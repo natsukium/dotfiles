2e594227420981865a1de98c15db5ebdb7d44e40
natsukium <tm.tomatoes@gmail.com>
2019-07-11 01:15:46 +0900

    Fix link script

--- bin/link.sh
+++ bin/link.sh
@@ -9,22 +9,22 @@
 XDG_CONFIG_HOME=$HOME/.config
 
 for file in .??*; do
-    [[ $file == ".git" ]] && continue
+    [ $file = ".git" ] && continue
     ln -snfv $PWD/$file $HOME/$file
 done
 
 for file in */.??*; do
-    [[ $(dirname $file) == "X11" ]] && continue
+    [ $(dirname $file) = "X11" ] && continue
     ln -snfv $PWD/$file $HOME/$(basename $file)
 done
 
 for file in */link.sh; do
-    [[ $(dirname $file) == "bin" ]] && continue
+    [ $(dirname $file) = "bin" ] && continue
     XDG_CONFIG_HOME=$HOME/.config ./$file
 done
 
 for dir in xdg_config_home/*; do
-    [[ ! -d $XDG_CONFIG_HOME/$(basename $dir) ]] && mkdir -p $XDG_CONFIG_HOME/$(basename $dir)
+    [ ! -d $XDG_CONFIG_HOME/$(basename $dir) ] && mkdir -p $XDG_CONFIG_HOME/$(basename $dir)
     for file in $dir/*; do
         [[ $file == "xdg_config_home/fish/functions" ]] &&
             ln -snfv $PWD/$file/* $XDG_CONFIG_HOME/fish/functions && continue
--- vscode/link.sh
+++ vscode/link.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-if [ "$(uname)" == 'Darwin' ]; then
+if [ "$(uname)" = 'Darwin' ]; then
     VSCODE_SETTING_PATH=$HOME/Library/Application\ Support/Code/User
-elif [ "$(uname)" == 'Linux' ]; then
+elif [ "$(uname)" = 'Linux' ]; then
     VSCODE_SETTING_PATH=$HOME/.config/Code/User
 fi
-[[ ! -d $VSCODE_SETTING_PATH ]] && mkdir -p $VSCODE_SETTING_PATH
+[ ! -d $VSCODE_SETTING_PATH ] && mkdir -p $VSCODE_SETTING_PATH
 
 ln -snfv $PWD/vscode/settings.json "$VSCODE_SETTING_PATH/settings.json"
