075d76f88754167b3071eab1433b5415ab6e2b9b
natsukium <tm.tomatoes@gmail.com>
2020-04-26 11:05:22 +0900

    Fix symbolic link

--- bin/link.sh
+++ bin/link.sh
@@ -26,8 +26,13 @@ done
 for dir in xdg_config_home/*; do
     [ ! -d $XDG_CONFIG_HOME/$(basename $dir) ] && mkdir -p $XDG_CONFIG_HOME/$(basename $dir)
     for file in $dir/*; do
-        [ $file = "xdg_config_home/fish/functions" ] &&
-            ln -snfv $PWD/$file/* $XDG_CONFIG_HOME/fish/functions && continue
+        if [ $file = "xdg_config_home/fish/functions" ]; then
+            [ ! -d $XDG_CONFIG_HOME/fish/functions ] && mkdir -p $XDG_CONFIG_HOME/fish/functions
+            for function in $file/*; do
+                ln -snfv $PWD/$function $XDG_CONFIG_HOME/fish/functions/$(basename $function)
+            done
+            continue
+        fi
         ln -snfv $PWD/$file $XDG_CONFIG_HOME/$(basename $dir)/$(basename $file)
     done
 done
