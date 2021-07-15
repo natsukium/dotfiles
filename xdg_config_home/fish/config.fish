043ae97a4756cfbe75c4312a86e03aa1f749609b
natsukium <tomoya.otabi@gmail.com>
2021-07-16 00:31:09 +0900

    Update fisher

--- xdg_config_home/fish/config.fish
+++ xdg_config_home/fish/config.fish
@@ -1,7 +1,6 @@
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
+    curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 end
 
 bind \cs 'fzfz'
