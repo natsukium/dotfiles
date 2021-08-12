53857fecf3ba333e10388dc6acb427aba4469f77
natsukium <tomoya.otabi@gmail.com>
2021-08-12 10:57:19 +0900

    Migrate to Nix (git)

---
+++ nix/home.nix
@@ -0,0 +1,10 @@
+{ pkgs, ... }: {
+  programs.home-manager.enable = true;
+  home = {
+    username = "$USER";
+    homeDirectory = "/Users/$USER";
+    stateVersion = "21.05";
+  };
+
+  imports = [ ./git ];
+}
