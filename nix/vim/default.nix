a2ca2079f2b05a96836ff77c4345a5a81594d911
natsukium <tomoya.otabi@gmail.com>
2021-08-13 17:58:16 +0900

    Migrate to Nix (vim)

---
+++ nix/vim/default.nix
@@ -0,0 +1,10 @@
+{ pkgs, ... }:
+
+{
+  programs.vim.enable = false; # if true, vim cannot read $XDG_CONFIG_HOME/vim/vimrc
+  home.packages = [ pkgs.vim ];
+  xdg.configFile."vim/vimrc".source = ./vimrc; # vimrc is read by vscode of Windows too
+  programs.bash.profileExtra = ''
+    export VIMINIT='source $XDG_CONFIG_HOME/vim/vimrc'
+  '';
+}
