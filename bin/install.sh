da3d415eb9f2af3a4792ffae5d28e719fc8d37a7
natsukium <tomoya.otabi@gmail.com>
2021-08-15 12:59:57 +0900

    Replace install scripts

--- bin/install.sh
+++ bin/install.sh
@@ -1,29 +1,19 @@
 #!/bin/sh
 
-#     ____           __        ____        __
-#    /  _/___  _____/ /_____ _/ / /  _____/ /_
-#    / // __ \/ ___/ __/ __ `/ / /  / ___/ __ \
-#  _/ // / / (__  ) /_/ /_/ / / /_ (__  ) / / /
-# /___/_/ /_/____/\__/\__,_/_/_/(_)____/_/ /_/
-
 if type git >/dev/null 2>&1; then
-    git clone https://github.com/natsukium/dotfiles $HOME/.dotfiles
+  git clone https://github.com/natsukium/dotfiles $HOME/.dotfiles
 else
-    tarball="https://github.com/natsukium/dotfiles/archive/master.tar.gz"
-    curl -L "$tarball" | tar xvz
-    mv -f dotfiles-master $HOME/.dotfiles
-
-    echo "Please install git."
-    exit
+  tarball="https://github.com/natsukium/dotfiles/archive/master.tar.gz"
+  curl -L "$tarball" | tar xvz
+  mv -f dotfiles-master $HOME/.dotfiles
 fi
 
 if test $REMOTE_CONTAINERS; then
-    make container
+  make container
 else
-    cd $HOME/.dotfiles
-    make minimum
-
-    if [ $1 = "--local" ]; then
-        make local
-    fi
+  cd $HOME/.dotfiles
+  . bin/install_nix.sh
+  install_nix
+  install_nix_unstable
+  install_home_manager
 fi
