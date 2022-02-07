e40833b323bebb5df0607c920d8615cc8daa36a6
natsukium <tomoya.otabi@gmail.com>
2022-02-07 23:08:43 +0900

    chore(script): shellcheck

--- bin/install_nix.sh
+++ bin/install_nix.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-XDG_CONFIG_HOME=$HOME/.config
-
 install_nix() {
   if [ "$(uname)" = 'Darwin' ]; then
     curl -L https://nixos.org/nix/install | sh -s -- --daemon
+    # shellcheck disable=SC1091
     . /nix/var/nix/profiles/default/etc/profile.d/nix.sh
   else
     curl -L https://nixos.org/nix/install | sh -s -- --no-daemon
-    . $HOME/.nix-profile/etc/profile.d/nix.sh
+    # shellcheck disable=SC1091
+    . "$HOME"/.nix-profile/etc/profile.d/nix.sh
   fi
   nix-channel --add https://nixos.org/channels/nixpkgs-unstable
   nix-channel --update
@@ -17,7 +17,7 @@ install_nix() {
 install_home_manager() {
   nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
   nix-channel --update
-  export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
+  export NIX_PATH="$HOME"/.nix-defexpr/channels${NIX_PATH:+:}"$NIX_PATH"
   nix-shell '<home-manager>' -A install
 }
 
