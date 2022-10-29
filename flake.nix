279fcdc41e2310930d85e89581c60a9a1ca727da
natsukium <tomoya.otabi@gmail.com>
2022-10-29 17:24:43 +0900

    fix: suppress 'warning: ignoring untrusted substituter'

--- flake.nix
+++ flake.nix
@@ -74,6 +74,11 @@
                 };
                 users.users."tomoya.matsumoto".home = "/Users/tomoya.matsumoto";
                 services.nix-daemon.enable = true;
+                nix.settings = {
+                  substituters = ["https://cache.nixos.org" "https://natsukium.cachix.org"];
+                  trusted-public-keys = ["cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" "natsukium.cachix.org-1:STD7ru7/5+KJX21m2yuDlgV6PnZP/v5VZWAJ8DZdMlI="];
+                  trusted-users = ["root" "@wheel"];
+                };
                 homebrew = {
                   enable = true;
                   brews = [
--- nix/nix/default.nix
+++ nix/nix/default.nix
@@ -9,7 +9,7 @@
       sandbox = true;
       substituters = ["https://cache.nixos.org" "https://natsukium.cachix.org"];
       trusted-public-keys = ["cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" "natsukium.cachix.org-1:STD7ru7/5+KJX21m2yuDlgV6PnZP/v5VZWAJ8DZdMlI="];
-      trusted-substituters = "https://natsukium.cachix.org";
+      trusted-users = ["root" "@wheel"];
     };
   };
   nixpkgs.config = {allowUnfree = true;};
