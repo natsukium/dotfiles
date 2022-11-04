4e4b28baa0e29499ceb2b0c49b07873d036a9b31
natsukium <tomoya.otabi@gmail.com>
2022-11-04 13:53:42 +0000

    feat(win): add openssh

--- win/main.ps1
+++ win/main.ps1
@@ -31,6 +31,13 @@ $baseUrl = "https://raw.githubusercontent.com/natsukium/dotfiles/master/win/"
 # Setup WSL
 wsl --install
 
+# Enable sshd
+Add-WindowsCapability -Online -Name OpenSSH.Client
+Add-WindowsCapability -Online -Name OpenSSH.Server
+Start-Service sshd
+Set-Service -Name sshd -StartupType 'Automatic'
+New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Program Files\PowerShell\7\pwsh.exe" -PropertyType String -Force
+
 # Remove unnecessary apps
 excuteScript ($baseUrl + "del_default_apps.ps1")
 
