527916438ca2a664c0b7a0d0d64aa45ae613158d
natsukium <tomoya.otabi@gmail.com>
2023-02-21 22:31:57 +0900

    feat: add ghq root for GH Enterprise

--- nix/ghq/default.nix
+++ nix/ghq/default.nix
@@ -4,5 +4,6 @@
     ghq.root = "~/src/private";
     "ghq \"ssh://git@gitlab.com/exwzd\"".root = "~/src/work";
     "ghq \"ssh://git@gitlab.com/tomoya.matsumoto\"".root = "~/src/work";
+    "ghq \"ssh://git@github.com-emu\"".root = "~/src/work";
   };
 }
