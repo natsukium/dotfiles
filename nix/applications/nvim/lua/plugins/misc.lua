53cc20279c2542b201de3d7cf76e4ea718b54ffa
natsukium <tomoya.otabi@gmail.com>
2023-11-03 14:59:36 +0900

    chore(nvim): add trouble.nvim integration for telescope.nvim

--- nix/applications/nvim/lua/plugins/misc.lua
+++ nix/applications/nvim/lua/plugins/misc.lua
@@ -30,11 +30,23 @@ return {
     cmd = "Telescope",
     keys = { "<leader>f" },
     config = function()
+      local telescope = require("telescope")
+      local trouble = require("trouble.providers.telescope")
       local builtin = require("telescope.builtin")
+
       vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
       vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
       vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+      telescope.setup({
+        defaults = {
+          mappings = {
+            i = { ["<c-t>"] = trouble.open_with_trouble },
+            n = { ["<c-t>"] = trouble.open_with_trouble },
+          },
+        },
+      })
     end,
   },
   {
