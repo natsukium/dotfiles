ae67120f3064ada84fcca0e11892298d8ea97b8a
natsukium <tomoya.otabi@gmail.com>
2023-12-22 12:10:59 +0900

    chore: set ssl-cert-file

--- nix/systems/darwin/work.nix
+++ nix/systems/darwin/work.nix
@@ -9,4 +9,13 @@
     enable = true;
     brews = [ "libomp" ];
   };
+
+  nix.extraOptions = ''
+    ssl-cert-file = /Library/Application Support/Netskope/STAgent/download/nscacert.pem
+  '';
+
+  environment.variables = {
+    REQUESTS_CA_BUNDLE = "/Library/Application Support/Netskope/STAgent/download/nscacert.pem";
+    NODE_EXTRA_CA_CERTS = "/Library/Application Support/Netskope/STAgent/download/nscacert.pem";
+  };
 }
