a9576614e7a4c818b4e81f790d061ea5519144a6
natsukium <tomoya.otabi@gmail.com>
2024-01-09 00:29:12 +0900

    chore(flake): use nixpkgs-unstable instead of master for stability

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "dotfiles";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
