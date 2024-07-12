ab5dbcb23ba977c4b979dbdf33b3be426db0d495
natsukium <tomoya.otabi@gmail.com>
2024-07-12 18:57:39 +0900

    chore(hyprland): add some environment variables for nvidia

--- nix/applications/hyprland/default.nix
+++ nix/applications/hyprland/default.nix
@@ -16,6 +16,10 @@
 
       # Some default env vars.
       env = XCURSOR_SIZE,24
+      env = LIBVA_DRIVER_NAME,nvidia
+      env = XDG_SESSION_TYPE,wayland
+      env = GBM_BACKEND,nvidia-drm
+      env = __GLX_VENDOR_LIBRARY_NAME,nvidia
 
       # For all categories, see https://wiki.hyprland.org/Configuring/Variables/
       input {
