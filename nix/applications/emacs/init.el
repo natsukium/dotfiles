89b533e083a21c159a1361d1095607c5bb5ebd3f
natsukium <tomoya.otabi@gmail.com>
2024-08-09 00:27:55 +0900

    feat(emacs): enable org-roam

--- nix/applications/emacs/init.el
+++ nix/applications/emacs/init.el
@@ -25,6 +25,26 @@
 
 (setq org-src-preserve-indentation t)
 
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-directory "~/dropbox/org-roam")
+  (org-roam-db-location "~/.local/share/org-roam.db")
+  :bind
+  (("C-c n l" . org-roam-buffer-toggle)
+   ("C-c n f" . org-roam-node-find)
+   ("C-c n g" . org-roam-graph)
+   ("C-c n i" . org-roam-node-insert)
+   ("C-c n c" . org-roam-capture)
+   ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  (setq org-roam-node-display-template
+        (concat "${title:*} "
+                (propertize "${tags:10}" 'face 'org-tag)))
+  (org-roam-db-autosync-mode)
+  (require 'org-roam-protocol)
+  )
+
 (use-package nix-mode
   :ensure t
   :mode "\\.nix\\'")
