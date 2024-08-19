79eb2b29f6538c2431aa66e3a0c766a06ab872cd
natsukium <tomoya.otabi@gmail.com>
2024-08-19 17:46:43 +0900

    chore(vim): remove unnecessary xdg hack
    Vim now respects XDG_CONFIG_HOME

--- nix/applications/vim/default.nix
+++ nix/applications/vim/default.nix
@@ -1,9 +1,5 @@
 { pkgs, ... }:
 {
-  programs.vim.enable = false; # if true, vim cannot read $XDG_CONFIG_HOME/vim/vimrc
-  home.packages = [ pkgs.vim ];
+  programs.vim.enable = true;
   xdg.configFile."vim/vimrc".source = ./vimrc; # vimrc is read by vscode of Windows too
-  home.sessionVariablesExtra = ''
-    export VIMINIT='let $MYVIMRC = !has("nvim") ? "$XDG_CONFIG_HOME/vim/vimrc" : "$XDG_CONFIG_HOME/nvim/init.lua" | so $MYVIMRC'
-  '';
 }
