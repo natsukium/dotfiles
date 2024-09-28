839316e8d6639310297ce5140eef5fc3269489fe
natsukium <tomoya.otabi@gmail.com>
2024-09-28 23:51:25 +0900

    refactor(qutebrowser): remove old setting

--- applications/qutebrowser/default.nix
+++ applications/qutebrowser/default.nix
@@ -4,15 +4,7 @@
     enable = true;
     settings = {
       content.blocking.method = "both";
-      window.hide_decoration =
-        # yabai cannot control qutebrowser with Qt5
-        # relate: https://github.com/qutebrowser/qutebrowser/issues/4067
-        if (pkgs.stdenv.isLinux) then
-          true
-        else if (pkgs.lib.versionOlder config.programs.qutebrowser.package.version "2.6.0") then
-          false
-        else
-          true;
+      window.hide_decoration = true;
     };
   };
 }
