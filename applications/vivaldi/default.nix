391497afa37ac4cf8ce17f16e338ef2b7f7a0ce7
natsukium <tomoya.otabi@gmail.com>
2024-10-16 23:09:02 +0900

    fix: use pkgs.vivaldi if platform is darwin

--- applications/vivaldi/default.nix
+++ applications/vivaldi/default.nix
@@ -3,14 +3,18 @@
   programs.chromium = {
     enable = true;
     # use snapshot to enable text-input-version 3 support
-    package = (pkgs.vivaldi.override { isSnapshot = true; }).overrideAttrs (oldAttrs: {
-      version = "6.10.3491.4";
-      src = pkgs.fetchurl {
-        url = "https://downloads.vivaldi.com/snapshot/vivaldi-snapshot_6.10.3491.4-1_amd64.deb";
-        hash = "sha256-jbDLdcyjwrsvmSM1MbPSeGMtcewiSDDKzN4nVYo/V/U=";
-      };
-    });
-    enableOzone = true;
+    package =
+      if pkgs.stdenv.hostPlatform.isLinux then
+        (pkgs.vivaldi.override { isSnapshot = true; }).overrideAttrs (oldAttrs: {
+          version = "6.10.3491.4";
+          src = pkgs.fetchurl {
+            url = "https://downloads.vivaldi.com/snapshot/vivaldi-snapshot_6.10.3491.4-1_amd64.deb";
+            hash = "sha256-jbDLdcyjwrsvmSM1MbPSeGMtcewiSDDKzN4nVYo/V/U=";
+          };
+        })
+      else
+        pkgs.vivaldi;
+    enableOzone = pkgs.stdenv.hostPlatform.isLinux;
     commandLineArgs = pkgs.lib.optionals config.programs.chromium.enableOzone [
       "--enable-wayland-ime"
       "--wayland-text-input-version=3"
