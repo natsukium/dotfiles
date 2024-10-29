3c219efb19a16c0e0681dec6a9f012bb3182b6bd
natsukium <tomoya.otabi@gmail.com>
2024-10-29 12:23:47 +0900

    fix: pick python packages fix from staging-next

--- overlays/default.nix
+++ overlays/default.nix
@@ -13,4 +13,22 @@
       bitwarden-cli
       ;
   };
+
+  temporary-fix = final: prev: {
+    python3 = prev.python3.override {
+      packageOverrides = pyfinal: pyprev: {
+        pyreqwest-impersonate = pyprev.pyreqwest-impersonate.overridePythonAttrs (oldAttrs: {
+          # https://github.com/NixOS/nixpkgs/issues/349432#issuecomment-2423129494
+          postFixup =
+            (oldAttrs.postFixup or "")
+            + final.lib.optionalString final.stdenv.hostPlatform.isLinux ''
+              ${final.patchelf}/bin/patchelf --add-rpath ${final.gcc-unwrapped.lib}/lib --add-needed libstdc++.so.6 $out/lib/python3.12/site-packages/pyreqwest_impersonate/pyreqwest_impersonate.cpython-312-x86_64-linux-gnu.so
+            '';
+        });
+        opentelemetry-proto = pyprev.opentelemetry-proto.overridePythonAttrs (oldAttrs: {
+          pythonRelaxDeps = (oldAttrs.pythonRelaxDeps or [ ]) ++ [ "protobuf" ];
+        });
+      };
+    };
+  };
 }
