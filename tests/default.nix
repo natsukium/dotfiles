4dbb9c572e73c768c5a49e9a544f02145e1187c8
natsukium <tomoya.otabi@gmail.com>
2024-11-10 11:25:38 +0900

    refactor(test): expose only if linux systems

--- tests/default.nix
+++ tests/default.nix
@@ -2,7 +2,7 @@
 let
   nixos-lib = import "${nixpkgs}/nixos/lib" { };
 in
-{
+pkgs.lib.optionalAttrs (pkgs.stdenv.hostPlatform.isLinux) {
   nixosModuleTests = nixos-lib.runTest {
     hostPkgs = pkgs;
 
