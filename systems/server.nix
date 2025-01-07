b1294ec8602874392ee3d04da28e937f6d6d6b01
natsukium <tomoya.otabi@gmail.com>
2025-01-08 00:16:40 +0900

    feat: disable more documents for server

--- systems/server.nix
+++ systems/server.nix
@@ -1,4 +1,17 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
+
+let
+  inherit (lib) mkDefault;
+in
 {
-  documentation.enable = lib.mkDefault false;
+  documentation =
+    {
+      enable = mkDefault false;
+      doc.enable = mkDefault false;
+      info.enable = mkDefault false;
+      man.enable = mkDefault false;
+    }
+    // lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
+      nixos.enable = mkDefault false;
+    };
 }
