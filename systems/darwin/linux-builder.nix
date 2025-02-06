614cd032ac36c5afa8f73ec9383261584f00fa28
natsukium <tomoya.otabi@gmail.com>
2025-02-07 01:03:37 +0900

    fix(darwin): add workaround for linux-builder

--- systems/darwin/linux-builder.nix
+++ systems/darwin/linux-builder.nix
@@ -1,6 +1,12 @@
+{ pkgs, ... }:
+let
+  inherit (pkgs) stdenv;
+in
 {
   nix.linux-builder = {
     enable = true;
     ephemeral = true;
+    # workaround for https://github.com/LnL7/nix-darwin/pull/1319
+    systems = [ stdenv.hostPlatform.system ];
   };
 }
