99311080e3cc71e0913784945c579e3a7c913d49
natsukium <tomoya.otabi@gmail.com>
2025-02-11 01:12:47 +0900

    feat(emacs): init.org init

--- applications/emacs/default.nix
+++ applications/emacs/default.nix
@@ -1,8 +1,31 @@
-{ pkgs, config, ... }:
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
 let
+  emacs-unwrapped =
+    if pkgs.stdenv.hostPlatform.isDarwin then pkgs.emacs30-plus else pkgs.emacs30-pgtk;
+
+  tangle =
+    org:
+    let
+      stem = path: lib.head (lib.splitString "." path);
+      outName = "${stem (builtins.baseNameOf org)}.el";
+    in
+    pkgs.runCommandNoCC "${outName}" { nativeBuildInputs = [ emacs-unwrapped ]; } ''
+      cp ${org} tmp.org
+      emacs -Q --batch --eval \
+        "(progn 
+          (require 'ob-tangle)
+          (org-babel-tangle-file \"tmp.org\" \"emacs-lisp\"))"
+      install emacs-lisp $out
+    '';
+
   emacs = pkgs.emacsWithPackagesFromUsePackage {
-    package = if pkgs.stdenv.isDarwin then pkgs.emacs30-plus else pkgs.emacs30-pgtk;
-    config = ./init.el;
+    package = emacs-unwrapped;
+    config = tangle ./init.org;
     extraEmacsPackages = epkgs: with epkgs; [ ];
   };
 in
@@ -12,7 +35,7 @@ in
     package = emacs;
   };
 
-  xdg.configFile."emacs/init.el".source = ./init.el;
+  xdg.configFile."emacs/init.el".source = tangle ./init.org;
 
   home.shellAliases = pkgs.lib.optionalAttrs pkgs.stdenv.isDarwin {
     emacs = "${config.programs.emacs.package}/Applications/Emacs.app/Contents/MacOS/Emacs";
