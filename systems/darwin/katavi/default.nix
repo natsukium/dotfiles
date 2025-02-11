0d2eae6b15f36105566bcfae11f59f4df6223447
natsukium <tomoya.otabi@gmail.com>
2025-02-12 00:04:11 +0900

    fix(katavi): update uid

--- systems/darwin/katavi/default.nix
+++ systems/darwin/katavi/default.nix
@@ -1,10 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, specialArgs, ... }:
 {
   imports = [
     ../common.nix
     ../desktop.nix
     ../linux-builder.nix
   ];
+
+  users.users.${specialArgs.username}.uid = 502;
 
   networking.hostName = "katavi";
 }
