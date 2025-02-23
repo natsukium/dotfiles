9d3dd88171e05deb3f3bea8c69b76e85cde68489
natsukium <tomoya.otabi@gmail.com>
2025-02-23 10:42:52 +0900

    fix(neomutt): sync with gmail's trash and so on

--- homes/shared/email.nix
+++ homes/shared/email.nix
@@ -15,6 +15,15 @@
         enable = true;
         mailboxType = "imap";
       };
+      # to sync with GMail's trash, I need to add the label like "+[Gmail]/ゴミ箱"
+      # but labels with Japanese characters are not supported by neomutt
+      # so I set the display language to English(US) in GMail settings
+      folders = {
+        inbox = "Inbox";
+        sent = "\[Gmail\]/Sent\\ Mail";
+        trash = "\[Gmail\]/Trash";
+        drafts = "\[Gmail\]/Drafts";
+      };
     };
   };
 
