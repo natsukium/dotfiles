6d9f79ead3317150fcfe18ed625ac98aa8c25ac0
natsukium <tomoya.otabi@gmail.com>
2025-04-11 10:57:18 +0900

    fix: linux evaluation

--- homes/shared/claude.nix
+++ homes/shared/claude.nix
@@ -5,34 +5,33 @@
   ...
 }:
 let
-  inherit (pkgs) lib stdenv;
+  inherit (pkgs) stdenv;
   inherit (inputs) mcp-servers;
 in
 {
-  home.file."Library/Application Support/Claude/claude_desktop_config.json" =
-    lib.optionalAttrs stdenv.hostPlatform.isDarwin
-      {
-        source = mcp-servers.lib.mkConfig pkgs {
-          programs = {
-            fetch.enable = true;
-            playwright.enable = true;
-            filesystem = {
-              enable = true;
-              args = [ "${config.programs.git.extraConfig.ghq.root}/github.com/natsukium" ];
-            };
-            github = {
-              enable = true;
-              envFile = config.sops.secrets.gh-token-for-mcp.path;
-            };
-          };
-
-          settings.servers.duckduckgo-search = {
-            command = "${pkgs.lib.getExe' pkgs.uv "uvx"}";
-            args = [ "duckduckgo-mcp-server" ];
-          };
+  home.file."Library/Application Support/Claude/claude_desktop_config.json" = {
+    enable = stdenv.hostPlatform.isDarwin;
+    source = mcp-servers.lib.mkConfig pkgs {
+      programs = {
+        fetch.enable = true;
+        playwright.enable = true;
+        filesystem = {
+          enable = true;
+          args = [ "${config.programs.git.extraConfig.ghq.root}/github.com/natsukium" ];
         };
-
+        github = {
+          enable = true;
+          envFile = config.sops.secrets.gh-token-for-mcp.path;
+        };
       };
+
+      settings.servers.duckduckgo-search = {
+        command = "${pkgs.lib.getExe' pkgs.uv "uvx"}";
+        args = [ "duckduckgo-mcp-server" ];
+      };
+    };
+
+  };
 
   sops.secrets.gh-token-for-mcp = {
     sopsFile = ./secrets.yaml;
