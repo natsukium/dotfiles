1c94a43a105501d476115972d225c3b7f0fe1247
natsukium <tomoya.otabi@gmail.com>
2025-05-06 00:31:11 +0900

    chore: remove unused overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,12 +2,6 @@
 {
   stable = final: prev: {
     inherit (inputs.nixpkgs-stable.legacyPackages.${final.stdenv.hostPlatform.system})
-      # swift is broken on darwin as of 2024-06
-      # https://github.com/NixOS/nixpkgs/issues/320900
-      swift
-      swiftPackages
-      swiftpm
-      swiftpm2nix
       # https://github.com/NixOS/nixpkgs/issues/339576
       bitwarden-cli
       ;
@@ -18,10 +12,6 @@
       packageOverrides = pyfinal: pyprev: {
         rapidocr-onnxruntime = pyprev.rapidocr-onnxruntime.overridePythonAttrs (_: {
           # segmentation fault
-          doCheck = false;
-        });
-        chromadb = pyprev.chromadb.overridePythonAttrs (_: {
-          # raise httpx.ReadError
           doCheck = false;
         });
       };
