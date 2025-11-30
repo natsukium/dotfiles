3d9202d955b4d739ac430fe07d3c42f65d6a6eb7
natsukium <tomoya.otabi@gmail.com>
2025-12-01 02:30:20 +0900

    docs(overlays): migrate to literate configuration

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,7 +2,6 @@
 {
   stable = final: prev: {
     inherit (inputs.nixpkgs-stable.legacyPackages.${final.stdenv.hostPlatform.system})
-      # https://github.com/NixOS/nixpkgs/pull/463879
       hercules-ci-agent
       ;
   };
@@ -11,12 +10,9 @@
     python313 = prev.python313.override {
       packageOverrides = pyfinal: pyprev: {
         rapidocr-onnxruntime = pyprev.rapidocr-onnxruntime.overridePythonAttrs (_: {
-          # segmentation fault
           doCheck = false;
         });
         lxml-html-clean = pyprev.lxml-html-clean.overridePythonAttrs (_: {
-          # test failures with libxml2 2.14
-          # https://github.com/fedora-python/lxml_html_clean/issues/24
           doCheck = false;
         });
       };
@@ -26,8 +22,6 @@
   pre-release = final: prev: { };
 
   patches = final: prev: {
-    # preview pane is corrupted when `LANG=ja_JP.UTF-8`
-    # https://github.com/dlvhdr/gh-dash/issues/316
     gh-dash =
       (final.writeShellApplication {
         name = "gh-dash";
@@ -35,15 +29,7 @@
           LANG=C.UTF-8 ${final.lib.getExe prev.gh-dash} "$@"
         '';
       }).overrideAttrs
-        # workaround for `attribute 'pname' missing
-        # at /nix/store/cj9d242mdxb7n217gs9x2yfxpydlg2q6-source/modules/programs/gh.nix:160:9:
-        #  159|       source = pkgs.linkFarm "gh-extensions" (builtins.map (p: {
-        #  160|         name = p.pname;
-        #     |         ^
-        #  161|         path = "${p}/bin";
-        #  error: attribute 'pname' missing
         { pname = "gh-dash"; };
-
     inherit (final.callPackage ../pkgs/mkShim { }) mkShim commandLineToolsShim;
   };
 }
