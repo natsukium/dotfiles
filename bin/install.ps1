1d3648a8ffb4ca89a0748894a45d6470fefad720
natsukium <tomoya.otabi@gmail.com>
2025-11-30 16:57:37 +0900

    chore: remove unused scripts

--- bin/install.ps1
+++
@@ -1,7 +0,0 @@
-Set-ExecutionPolicy RemoteSigned
-
-# Install Boxstarter
-. { Invoke-WebRequest -useb https://boxstarter.org/bootstrapper.ps1 } | Invoke-Expression; Get-Boxstarter -Force
-
-# Execute install scripts
-Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/natsukium/dotfiles/master/win/main.ps1 -DisableReboots
