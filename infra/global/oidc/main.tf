ad4ba57e0161e94e4c11663eb1d888821243eb43
natsukium <tomoya.otabi@gmail.com>
2026-03-07 12:22:09 +0900

    fix: add s3:DeleteObject to apply role for state lock cleanup
    S3 native locking (use_lockfile = true) creates a .tflock file that
    must be deleted when releasing the lock, requiring s3:DeleteObject.

--- infra/global/oidc/main.tf
+++ infra/global/oidc/main.tf
@@ -148,6 +148,7 @@ data "aws_iam_policy_document" "tfstate_write" {
     actions = [
       "s3:GetObject",
       "s3:PutObject",
+      "s3:DeleteObject",
     ]
     resources = ["arn:aws:s3:::natsukium-tfstate/global/*"]
   }
