8ec787cca331b46bbcb726af06fab35f7ba6d5e3
natsukium <tomoya.otabi@gmail.com>
2026-03-20 15:37:16 +0900

    refactor(modules): move project-specific home-manager modules into home/
    Project-specific modules (my.* namespace) are relocated from
    modules/home-manager/ into modules/home/ under functional categories:
    development (colima, ghq), desktop (copyq, hammerspoon), shell (just,
    starship-async), email (mbsync, neomutt), security (pizauth), and
    services (pueue).
    
    modules/home-manager/default.nix now explicitly re-exports the moved
    modules to preserve the existing import chain from homes/common.nix.

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,7 +1,14 @@
 {
-  imports = (
-    builtins.map (modules: ./. + "/${modules}") (
-      builtins.filter (x: x != "default.nix") (builtins.attrNames (builtins.readDir ./.))
-    )
-  );
+  imports = [
+    ../home/development/colima
+    ../home/development/ghq/module.nix
+    ../home/desktop/copyq
+    ../home/desktop/hammerspoon
+    ../home/shell/just
+    ../home/shell/starship-async
+    ../home/email/mbsync
+    ../home/email/neomutt
+    ../home/security/pizauth
+    ../home/services/pueue
+  ];
 }
